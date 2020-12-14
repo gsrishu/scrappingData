880dac4864959b5933b4885f19d2be0a914c2aa7
Gaurav Singh <32643752+gsrishu@users.noreply.github.com>
2020-12-15 00:14:05 +0530

    concorrency and microservices

---
+++ lib/lib.go
@@ -0,0 +1,91 @@
+package lib
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+
+)
+type Product struct {
+	Name        string
+	ImageUrl    string
+	Description string
+	Price       string
+	TotalReview string
+}
+type User struct {
+	Url       string
+	Product   Product
+	CreatedAt time.Time
+}
+
+
+func Data(w http.ResponseWriter, r *http.Request) {
+
+
+	database, _ := sql.Open("sqlite3", "./amazonProductInfo.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, name VARCHAR , description VARCHAR, price VARCHAR, review VARCHAR, imageUrl VARCHAR, time VARCHAR)")
+	statement.Exec()
+
+	var user User
+	// json.Unmarshal([]byte(birdJson), &birds)
+	// fmt.Printf(bird)
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		panic(err)
+	}
+	userJson, err := json.Marshal(user)
+	if err != nil {
+		panic(err)
+	}
+
+	fName := "data.csv"
+	file, err := os.OpenFile(fName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	//file, err := os.Create(fName)
+	if err != nil {
+
+		log.Fatalf("Not, err : %q", err)
+
+		return
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	//c := colly.NewCollector()
+	cTime := time.Now().Local()
+	var testStrings [][]string
+	testStrings = append(testStrings, []string{user.Product.Name, user.Product.Description, user.Product.Price, user.Product.TotalReview, user.Product.ImageUrl, cTime.String()})
+	writer.WriteAll(testStrings)
+	statement, _ = database.Prepare("INSERT INTO data (name, description, price, review,imageUrl, time) VALUES (?, ?, ?, ?, ?, ?)")
+	statement.Exec(user.Product.Name,user.Product.Description, user.Product.Price, user.Product.TotalReview, user.Product.ImageUrl, cTime.String())
+
+	rows, _ := database.Query("SELECT id,name, description, price, review,imageUrl, time FROM data")
+	var id int
+	var name string
+	var description string
+	var price string
+	var review string
+	var imageUrl string
+	var time string
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &description, &price, &review, &imageUrl, &time)
+		fmt.Println(strconv.Itoa(id) + ": " + name + " " + description + " " + price + " " + review + " " + imageUrl + " " + time)
+	}
+
+	fmt.Printf(user.Product.Name)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//Write json response back to response
+	w.Write(userJson)
+}
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"gsr/lib"
 	"log"
 	"net/http"
 	"os"
@@ -17,24 +18,11 @@ import (
 
 )
 
-type Product struct {
-	Name        string
-	ImageUrl    string
-	Description string
-	Price       string
-	TotalReview string
-}
-type User struct {
-	Url       string
-	Product   Product
-	CreatedAt time.Time
-}
-
 func main() {
 
 	router := mux.NewRouter()
-	router.HandleFunc("/amazonData", amzData)
-	router.HandleFunc("/jsonData", jsonData)
+	go router.HandleFunc("/amazonData", amzData)
+	router.HandleFunc("/jsonData",lib.Data)
 	http.ListenAndServe(":5000", router)
 
 }
@@ -115,65 +103,8 @@ func amzData(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("SCRAPPING COMPLEATED AND DATA IS SAVED IN DB")
 }
 
-func jsonData(w http.ResponseWriter, r *http.Request) {
+// func jso(w http.ResponseWriter, r *http.Request) {
 
+// 	L.jsonData(w, r )
 
-	database, _ := sql.Open("sqlite3", "./amazonProductInfo.db")
-	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, name VARCHAR , description VARCHAR, price VARCHAR, review VARCHAR, imageUrl VARCHAR, time VARCHAR)")
-	statement.Exec()
-
-	var user User
-	// json.Unmarshal([]byte(birdJson), &birds)
-	// fmt.Printf(bird)
-	err := json.NewDecoder(r.Body).Decode(&user)
-	if err != nil {
-		panic(err)
-	}
-	userJson, err := json.Marshal(user)
-	if err != nil {
-		panic(err)
-	}
-
-	fName := "data.csv"
-	file, err := os.OpenFile(fName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
-	//file, err := os.Create(fName)
-	return
-	if err != nil {
-
-		log.Fatalf("Not, err : %q", err)
-
-		return
-	}
-
-	defer file.Close()
-
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
-	//c := colly.NewCollector()
-	cTime := time.Now().Local()
-	var testStrings [][]string
-	testStrings = append(testStrings, []string{user.Product.Name, user.Product.Description, user.Product.Price, user.Product.TotalReview, user.Product.ImageUrl, cTime.String()})
-	writer.WriteAll(testStrings)
-	statement, _ = database.Prepare("INSERT INTO data (name, description, price, review,imageUrl, time) VALUES (?, ?, ?, ?, ?, ?)")
-	statement.Exec(user.Product.Name,user.Product.Description, user.Product.Price, user.Product.TotalReview, user.Product.ImageUrl, cTime.String())
-
-	rows, _ := database.Query("SELECT id,name, description, price, review,imageUrl, time FROM data")
-	var id int
-	var name string
-	var description string
-	var price string
-	var review string
-	var imageUrl string
-	var time string
-
-	for rows.Next() {
-		rows.Scan(&id, &name, &description, &price, &review, &imageUrl, &time)
-		fmt.Println(strconv.Itoa(id) + ": " + name + " " + description + " " + price + " " + review + " " + imageUrl + " " + time)
-	}
-
-	fmt.Printf(user.Product.Name)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	//Write json response back to response
-	w.Write(userJson)
-}
+// }
